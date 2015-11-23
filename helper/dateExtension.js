30a45ecee832bc7e9759868f89708a46da89c474
D_ttang <D_ttang@163.com>
2015-11-23 23:54:21 +0800

    test npm install

--- helper/dateExtension.js
+++ helper/dateExtension.js
@@ -12,9 +12,10 @@ exports.format = function (date, fmt) {
     if (/(y+)/.test(fmt)) {
         fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
     }
-    for (let k in o)
+    for (let k in o) {
         if (new RegExp("(" + k + ")").test(fmt)) {
             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
         }
+    }
     return fmt;
 }
